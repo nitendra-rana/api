c41fd442605cf6114d4d25758e0233abfb089d1a
Nitendra Rana <nitendrarana108@gmail.com>
2022-11-01 02:21:27 +0530

    update origin

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import cors from 'cors'
 const app = express();
 const port = process.env.PORT || 5000
 app.use(cors({
-    origin: port,
+    origin: '*',
     method : ['GET', 'PUT', 'PATCH', 'DELETE']
 }));
 
