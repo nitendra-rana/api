db940cdf916f0383f1e1a4e0688ef230ede88737
Nitendra Rana <nitendrarana108@gmail.com>
2022-11-01 04:24:44 +0530

    cors test

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
-import express from "express"
-import movies from './movies.json' assert { type: "json" };
-import cors from 'cors'
+import express from "express";
+import movies from "./movies.json" assert { type: "json" };
+import cors from "cors";
 
 const app = express();
-const port = process.env.PORT || 5000
-var corsOptions = {
-    origin: '*',
-    optionsSuccessStatus: 200 
-  }
-app.use(cors([corsOptions]));
+const port = process.env.PORT || 5000;
+app.use(cors());
+app.options('*', cors());
 
 
-app.get( "/", (req, res) =>  res.send('hello from homepage')
-)
-app.get("/movies",cors([corsOptions]), (req, res) => {
-    res.send(movies);
-})
+app.get("/", (req, res) => res.send("hello from homepage"));
+app.get("/movies", (req, res, next) => {
+  res.send(movies);
+});
 
-app.listen(port, () => console.log(`server is running at ${port}`))
+app.listen(port, () => console.log(`server is running at ${port}`));
