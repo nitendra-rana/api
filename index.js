46a1b5874197dde8df67c5062a3b0f51781a4d8c
Nitendra Rana <nitendrarana108@gmail.com>
2022-11-01 17:19:39 +0530

    cors removed

--- index.js
+++ index.js
@@ -4,18 +4,15 @@ import cors from "cors";
 
 const app = express();
 app.use(express.json());
-app.use(cors({
-  origin: '*'
-}));
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 const port = process.env.PORT || 5000;
 
-
-app.head("/",cors(), (req, res) => res.send("hello from homepage"));
-app.get("/movies",cors({
-  origin: true
-}), (req, res, next) => {
+app.get("/movies", (req, res) => {
   res.status(200).json(movies);
-  next();
 });
 
 app.listen(port, () => console.log(`server is running at ${port}`));
