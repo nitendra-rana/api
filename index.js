a061d4081525853ad4b147299f8778f6d51d386e
Nitendra Rana <nitendrarana108@gmail.com>
2022-11-01 15:04:39 +0530

    method used

--- index.js
+++ index.js
@@ -4,20 +4,29 @@ import cors from "cors";
 
 const app = express();
 const port = process.env.PORT || 5000;
-app.use(cors());
-app.options('*', cors());
-router.get('/', function(req, res) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
-    res.setHeader('Access-Control-Allow-Credentials', true); // If needed
+app.use(function (req, res, next) {
 
-    res.send('cors problem fixed:)');
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
 });
 
 app.head("/",cors(), (req, res) => res.send("hello from homepage"));
-app.get("/movies",cors(), (req, res, next) => {
-  res.json(movies);
+app.get("/movies",cors([]), (req, res, next) => {
+  res.json(JSON.stringify(movies));
+  next();
 });
 
 app.listen(port, () => console.log(`server is running at ${port}`));
